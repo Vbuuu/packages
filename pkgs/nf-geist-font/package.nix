ab0308ceb3a780fbfe225c4aede72d557bc759b4
Vbuuu <87385391+Vbuuu@users.noreply.github.com>
2026-02-23 18:07:45 +0100

    Parallelize Geist Nerd Font build & do not patch GeistMono twice

--- pkgs/nf-geist-font/package.nix
+++ pkgs/nf-geist-font/package.nix
@@ -2,10 +2,12 @@
   stdenvNoCC,
   geist-font,
   nerd-font-patcher,
+  parallel,
+  lib,
   ...
 }:
 stdenvNoCC.mkDerivation {
-  name = "NF-Geist-Font";
+  name = "nerd-geist-font";
   version = (builtins.parseDrvName geist-font.name).version;
 
   nativeNuildInputs = [
@@ -22,8 +24,13 @@ stdenvNoCC.mkDerivation {
     runHook preInstall
 
     find ${geist-font}/share/fonts/opentype \
-      -name \*.otf \
-      -exec ${nerd-font-patcher}/bin/nerd-font-patcher --complete --quiet --no-progressbars {} \; \
-      -exec ${nerd-font-patcher}/bin/nerd-font-patcher --complete --use-single-width-glyphs --adjust-line-height --quiet --no-progressbars {} \;
+      -name \*.otf ! -name '*Mono*' -print0 | \
+      ${lib.getExe parallel} -j ''${NIX_BUILD_CORES:0} -u -0 \
+      ${lib.getExe nerd-font-patcher} --complete --quiet --no-progressbars {}
+
+    find ${geist-font}/share/fonts/opentype \
+      -name \*.otf -name '*Mono*' -print0 | \
+      ${lib.getExe parallel} -j ''${NIX_BUILD_CORES:0} -u -0 \
+      ${lib.getExe nerd-font-patcher} --complete --use-single-width-glyphs --adjust-line-height --quiet --no-progressbars {}
   '';
 }
